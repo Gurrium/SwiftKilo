e9d29dec185d0ee8b7068e1e3f8cb6dcae635c3c
gurrium <gurrium@hatena.ne.jp>
2023-02-21 20:48:37 +0900

    fix

--- Sources/SwiftKilo/SwiftKilo.swift
+++ Sources/SwiftKilo/SwiftKilo.swift
@@ -1,9 +1,13 @@
+import Foundation
+
 @main
 public struct SwiftKilo {
     public static func main() {
-        while let str = readLine(),
-              str != "q" {
-            print(str)
+        var char: UInt8 = 0
+        while read(FileHandle.standardInput.fileDescriptor, &char, 1) == 1,
+              let parsed = String(bytes: [char], encoding: .utf8),
+              parsed != "q" {
+            print(parsed)
         }
     }
 }
