e762968eef282fad91d9c30ea50eb9dd9536cffc
gurrium <gurrium@hatena.ne.jp>
2023-02-22 22:08:30 +0900

    next: https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html#turn-off-echoing#disable-raw-mode-at-exit

--- Sources/SwiftKilo/SwiftKilo.swift
+++ Sources/SwiftKilo/SwiftKilo.swift
@@ -3,11 +3,22 @@ import Foundation
 @main
 public struct SwiftKilo {
     public static func main() {
+        enableRawMode()
+
         var char: UInt8 = 0
         while read(FileHandle.standardInput.fileDescriptor, &char, 1) == 1,
               let parsed = String(bytes: [char], encoding: .utf8),
               parsed != "q" {
             print(parsed)
         }
+    }
+
+    private static func enableRawMode() {
+        var raw = termios()
+        tcgetattr(STDIN_FILENO, &raw)
+
+        raw.c_lflag &= tcflag_t(truncatingIfNeeded: ~(ECHO))
+
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)
     }
 }
